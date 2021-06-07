83b46b0a042fea9782df21a3a64b4aaec146f0fb
Viktorija Jenca <Lalukins23@gmail.com>
2021-06-07 14:48:30 +0300

    React lesson 14

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+const count =25;
+const flag = true;
+
 function App() {
   return (
     <div className="App">
@@ -9,8 +12,32 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style={{
+          color: '#7FF02E',
+          fontSize: '46px'
+        }}>
           Hello World ! :)
+        </p>
+        <p>
+          {count}
+        </p>
+        <p>
+          {null}
+          {undefined}
+          {true}
+          {false}
+        </p>
+        <p>
+          {flag && 'Flag is True'}
+        </p>
+        <p>
+          {flag ? 'Flag is True' : 'Flag is False'}
+        </p>
+        <p>
+          {2+5}
+        </p>
+        <p>
+          {777}
         </p>
         <a
           className="App-link"
