23558f479e684808277bb23ab23f5b7cfda444d2
Viktorija Jenca <Lalukins23@gmail.com>
2021-06-08 16:40:58 +0300

    Lesson 17

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const element = (<div>
-    Hello World!
-    <a href=""> Go to</a>
-  </div>);
-export {element };
+const ItemList = () => (<ul>
+  <li>1.One</li>
+  <li>2.Two</li>
+</ul>);
+const App = () => (<div>
+  <h1>Hello World!</h1>
+  <ItemList />
+</div>);
+
+
+export default App;
