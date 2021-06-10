2288dbb3aaf04d6f009f22531aec2812d0255a6c
Viktorija Jenca <Lalukins23@gmail.com>
2021-06-10 18:38:34 +0300

    React lesson 27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,30 +4,31 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css'
 
-  const todoItem = 'Write new aplication';
-  const App = () => {
-    const items = [
-      {
-        value: 'Write new aplication',
-        isDone: true
-      },
-      {
-        value: 'Write props',
-        isDone: false
-      },
-      {
-        value: 'Do all works',
-        isDone: true
-      }
-    ];
+  class App extends React.Component {
+      render() {
+        const items = [
+        {
+          value: 'Write new aplication',
+          isDone: true
+        },
+        {
+          value: 'Write props',
+          isDone: false
+        },
+        {
+          value: 'Do all works',
+          isDone: true
+        }
+      ];
 
     return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>TODOS:</h1>
-      <InputItem />
-      <ItemList items = {items} />
-      <Footer count = {1} />
-    </div>);
-}
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>TODOS:</h1>
+        <InputItem />
+        <ItemList items = {items} />
+        <Footer count = {1} />
+      </div>);
+    }
+  }
 
 export default App;
