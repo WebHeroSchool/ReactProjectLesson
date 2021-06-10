5ee887fec8bbf736a22da73c7b007f21326b87d8
Viktorija Jenca <Lalukins23@gmail.com>
2021-06-10 15:07:46 +0300

    React lesson 25 - Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-  const InputItem = () => (<span>InputItem</span>);
+  const InputItem = () => (<div>
+      <TextField id="standard-basic" label="Add what to do" />
+    </div>);
 
   export default InputItem;
