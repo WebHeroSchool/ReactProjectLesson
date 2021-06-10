3971d4d708aa533bbef7cd94ea4575a3eb41474d
Viktorija Jenca <Lalukins23@gmail.com>
2021-06-10 13:44:22 +0300

    React Lesson 22- adding css file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import './App.css'
 
   const todoItem = 'Write new aplication';
   const App = () => {
@@ -18,8 +19,8 @@ import Footer from '../Footer/Footer';
     ];
 
     return (
-    <div>
-      <h1>Important things or TODOS:</h1>
+    <div className="wrap">
+      <h1 className="wrap__title">Important things or TODOS:</h1>
       <InputItem />
       <ItemList items = {items} />
       <Footer count = {3} />
